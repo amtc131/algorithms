664b53845fce231b12ee6db1f1a66e4c8ba59799
amtc131 <amtc131@gmail.com>
2022-04-27 19:25:38 -0700

    initial commit

---
+++ module/num_in_list.go
@@ -0,0 +1,12 @@
+package module
+
+// NumInList will return true if num is in the
+// list slice, and false otherwiese.
+func NumInList(list []int, num int) bool {
+	for _, i := range list {
+		if i == num {
+			return true
+		}
+	}
+	return false
+}
