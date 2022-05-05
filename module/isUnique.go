61d1e245ffc3e1b2564f658a01a3c99cf2bcea2d
amtc131 <amtc131@gmail.com>
2022-05-04 20:04:37 -0700

    update problem, using character for determinate

--- module/isUnique.go
+++ module/isUnique.go
@@ -1,20 +1,32 @@
 package module
 
+// Is Unique implement an algoritm to determine if a string has all unique characters.
+// What if you cannot additional data structure
 func IsUnique(str string) bool {
-
-	k := 1
+	c := make([]bool, 128)
 
 	for i := 0; i < len(str); i++ {
-		for j := 1; j < len(str); j++ {
-			if string(str[i]) == string(str[j]) {
-				k++
-			}
-		}
-		if k <= 1 {
-			k = 0
-		} else {
+		val := str[i]
+		if c[val] {
 			return false
 		}
+		c[val] = true
 	}
+
 	return true
 }
+
+//	k := 1
+//	for i := 0; i < len(str); i++ {
+//		for j := 1; j < len(str); j++ {
+//			if string(str[i]) == string(str[j]) {
+//				k++
+//			}
+//		}
+//		if k <= 1 {
+//			k = 0
+//		} else {
+//			return false
+//		}
+//	}
+//	return true
