ecdd53100f2ba6d1a10879272fe863090498a60b
amtc131 <amtc131@gmail.com>
2022-05-12 20:31:16 -0500

    add module stack

--- structure/stack/stack.go
+++ structure/stack/stack.go
@@ -1,5 +1,7 @@
 package stack
 
+import "fmt"
+
 var size int
 var arr []int
 var top int
@@ -8,4 +10,43 @@ func MyStack(size int) {
 	size = size
 	arr = make([]int, size)
 	top = -1
+}
+
+func Push(element int) {
+	if !isFull() {
+		top++
+		arr[top] = element
+		fmt.Printf("Pushed element: %d", element)
+	} else {
+		fmt.Println("Stack is full")
+	}
+}
+
+func Pop() int {
+	if !isEmpty() {
+		topElement := top
+		top--
+		fmt.Printf("Popped element %d", arr[topElement])
+		return arr[topElement]
+	} else {
+		fmt.Println("Stack is empty!")
+		return -1
+	}
+}
+
+func Peek() int {
+	if !isEmpty() {
+		return arr[top]
+	} else {
+		fmt.Println("Stack is empty!")
+		return -1
+	}
+}
+
+func isFull() bool {
+	return (size-1 == top)
+}
+
+func isEmpty() bool {
+	return (top == -1)
 }
---
+++ structure/stack/test/stack_test.go
@@ -0,0 +1,19 @@
+package test
+
+import (
+	"testing"
+
+	stack "github.com/amtc131/algorithms/structure/stack"
+)
+
+func TestMyStack(t *testing.T) {
+	stack.MyStack(5)
+
+	t.Run("Test 1: Stack is empty", func(t *testing.T) {
+		got := stack.Pop()
+		if got != -1 {
+			t.Errorf("Pop() = %v, Got: %v", -1, got)
+		}
+	})
+
+}
