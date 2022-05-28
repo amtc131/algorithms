d80ebb7e9711143cdff505ce8497f9caa45fb89a
amtc131 <amtc131@gmail.com>
2022-05-27 20:45:36 -0500

    add new problems

--- c-structure/src/binarysearch.c
+++ c-structure/src/binarysearch.c
@@ -3,46 +3,64 @@
 #include <string.h>
 
 typedef struct binarysearch binarysearch;
-binarysearch* insert(binarysearch* root, int data);
+binarysearch *insert(binarysearch *root, int data);
+struct binarysearch *newNode(int data);
 
 struct binarysearch
 {
     int data;
-    binarysearch* left;
-    binarysearch* right;
+    struct binarysearch *left;
+    struct binarysearch *right;
 };
 
-binarysearch* insert(binarysearch* root, int data){
-    binarysearch* temp = NULL;
-    if(root == NULL){
-        temp = (binarysearch *)malloc(sizeof(binarysearch));
-        temp->data =data;
-        root = temp;
+struct binarysearch *newNode(int data)
+{
+    struct binarysearch *node = (struct binarysearch *)malloc(sizeof(struct binarysearch));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return (node);
+}
+
+binarysearch *insert(binarysearch *root, int data)
+{
+
+    if (root == NULL)
+    {
+        return newNode(data);
     }
 
-    if(root->left != NULL){
+    if (data < root->data)
+    {
         root->left = insert(root->left, data);
-    }else{
+    }
+    else
+    {
         root->right = insert(root->right, data);
     }
 
     return root;
 }
 
-void inOrder(binarysearch* root){
-    if(root != NULL){
+void inOrder(binarysearch *root)
+{
+    if (root != NULL)
+    {
         inOrder(root->left);
-        
+        printf("%d ", root->data);
         inOrder(root->right);
     }
 }
 
 int main(int argc, char const *argv[])
 {
-     binarysearch* tree = insert(NULL, 15);
-     tree = insert(tree, 10);
+    binarysearch *tree = insert(NULL, 15);
+    tree = insert(tree, 10);
+    tree = insert(tree, 20);
+    tree = insert(tree, 8);
+    tree = insert(tree, 12);
+    tree = insert(tree, 16);
+    tree = insert(tree, 25);
+    inOrder(tree);
     return 0;
 }
-
-
-
