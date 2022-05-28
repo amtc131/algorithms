e75333d5da712dfe883fc880b6b757f11c9a7462
amtc131 <amtc131@gmail.com>
2022-05-27 20:16:59 -0500

    add new problems

--- c-structure/src/binarysearch.c
+++ c-structure/src/binarysearch.c
@@ -1,7 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+typedef struct binarysearch binarysearch;
+binarysearch* insert(binarysearch* root, int data);
 
-main(){
+struct binarysearch
+{
+    int data;
+    binarysearch* left;
+    binarysearch* right;
+};
 
+binarysearch* insert(binarysearch* root, int data){
+    binarysearch* temp = NULL;
+    if(root == NULL){
+        temp = (binarysearch *)malloc(sizeof(binarysearch));
+        temp->data =data;
+        root = temp;
+    }
+
+    if(root->left != NULL){
+        root->left = insert(root->left, data);
+    }else{
+        root->right = insert(root->right, data);
+    }
+
+    return root;
+}
+
+void inOrder(binarysearch* root){
+    if(root != NULL){
+        inOrder(root->left);
+        
+        inOrder(root->right);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+     binarysearch* tree = insert(NULL, 15);
+     tree = insert(tree, 10);
+    return 0;
 }
 
 
